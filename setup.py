bf26af557b8e6fc5370fd12a5cb15e48d82f2f7d
JudSze <{ID}+{username}@users.noreply.github.com>
2025-07-08 17:26:34 +0200

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="halogenase_miner",
-    version="0.1.9",
+    version="0.1.10",
     description="A Python package to identify categorize and mine for halogenases",
     url="https://github.com/JudSze/hal_miner",
     author="Judit Szenei",
